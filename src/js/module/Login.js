3f781e7fe5599dc81429200ea1cebf53f062ab24
Viktor Ostapenko <vk.ostapenko@outlook.com>
2023-04-09 19:08:04 +0200

    add modules Card, Modal, Login

---
+++ src/js/module/Login.js
@@ -0,0 +1,36 @@
+import Request from "./Request.js";
+import { USER } from "./constans.js";
+
+export default class Login {
+  render() {
+    const form = document.createElement("form");
+    form.innerHTML =
+      '<h2 class="modal__name">Log in</h2>        <label class="modal__input-name">Login</label>        <input type="email" class="modal__input login-email" placeholder="Login" />        <label class="modal__input-name">Passwort</label>        <input type="password" class="modal__input login-password" placeholder="Password" /> <p class="modal__invalid-date">Incorrect username or password*</p>        <button class="modal__button">Log in</button>';
+
+    const loginBtn = form.querySelector(".modal__button");
+    const email = form.querySelector(".login-email");
+    const password = form.querySelector(".login-password");
+    const invalidText = form.querySelector('.modal__invalid-date');
+
+    loginBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const request = new Request();
+      request.login(email.value, password.value)
+      .then(token=>{
+        if(token !== 'Incorrect username or password'){
+            USER.token = token;
+            document.querySelector('.header__button--login').style.display = 'none';
+            document.querySelector('.header__button--visit').style.display = 'block';
+            document.querySelector('.modal').remove();
+            
+        }else{
+            invalidText.style.display = 'block';
+        }
+      })
+      .catch(error=>{
+        alert(error.message);
+      })
+    });
+    return form;
+  }
+}
